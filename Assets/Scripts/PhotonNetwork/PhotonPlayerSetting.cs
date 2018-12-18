463bfaf7ee6b2c82129d7a60bd35c04aff0af3ac
EricZhan <doremi31618@gmail.com>
2018-12-18 13:41:23 +0800

    sort out and update

---
+++ Assets/Scripts/PhotonNetwork/PhotonPlayerSetting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using System.Text;
+
+public class PhotonPlayerSetting : Photon.MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if(!photonView.isMine)
+        {
+            this.GetComponent<ThirdPersonUserControl>().enabled = false;
+            //this.GetComponent<ThirdPersonCharacter>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
