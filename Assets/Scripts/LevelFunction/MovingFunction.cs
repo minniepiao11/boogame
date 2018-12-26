e080478ac209ed0ce57563b1d775e09494aebe94
EricZhan <doremi31618@gmail.com>
2018-12-26 18:11:44 +0800

    Update TriggerToScale
    新增功能模塊：
    trigger 香菇
    觸碰後長高
    沒觸碰時下降

--- Assets/Scripts/LevelFunction/MovingFunction.cs
+++ Assets/Scripts/LevelFunction/MovingFunction.cs
@@ -90,6 +90,7 @@ public class MovingFunction : MonoBehaviour {
         }
     }
 
+
 }
 
 
---
+++ Assets/Scripts/LevelFunction/TriggerToScaleUp.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Build.Content;
+using System.Globalization;
+using System.Xml.Schema;
+
+public class TriggerToScaleUp : MonoBehaviour {
+
+    public GameObject ScaleTarget;
+
+    [Range(1,10)][SerializeField]
+    int magScaleY = 1;
+    [SerializeField][Header("長到最高需要的時間")]
+    float growingTime = 5f;
+    float time = 0;
+    private bool isGoing = true;
+    private bool _isTrigger;
+    public  bool isTrigger
+    {
+        get { return _isTrigger; }
+    }
+    private Vector3 original_scale;
+    Vector3 _scale;
+
+	// Use this for initialization
+	void Start () {
+        if (ScaleTarget == null)
+        {
+            Debug.LogError(ScaleTarget);
+            return;
+        }
+        original_scale = ScaleTarget.transform.localScale;
+	}
+
+    private void FixedUpdate()
+    {
+        if (ScaleTarget == null) 
+        {
+            Debug.LogError(ScaleTarget);
+            return;
+        }
+
+
+        if (_isTrigger)
+        {
+            _scale = Vector3.Lerp(original_scale, new Vector3(original_scale.x,original_scale.y * magScaleY,original_scale.z), time/growingTime);
+
+            if (time <= 1)
+            {
+                time += Time.deltaTime;
+            }
+        }
+        else
+        {
+            _scale = Vector3.Lerp(original_scale, new Vector3(original_scale.x, original_scale.y * magScaleY, original_scale.z), time / growingTime);
+
+            if (time >= 0)
+            {
+                time -= Time.deltaTime;
+            }
+        }
+
+        ScaleTarget.transform.localScale = _scale;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        _isTrigger = true;
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        _isTrigger = true;
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Player") return;
+        _isTrigger = false;
+
+    }
+}
