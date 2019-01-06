e3e10694c5810eb4a112064061bb1629729c2bcc
EricZhan <doremi31618@gmail.com>
2019-01-06 11:10:01 +0800

    Optimize collider & Tirgger to visible & Energy model

---
+++ Assets/Scripts/LevelFunction/TriggerToVisible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerToVisible : MonoBehaviour {
+    public GameObject[] VisibleTarget;
+	// Use this for initialization
+	void Start () {
+        foreach(GameObject _visibleObject in VisibleTarget)
+            _visibleObject.GetComponent<MeshRenderer>().enabled = false;
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!(other.gameObject.tag == "Player"))
+            return;
+        foreach (GameObject _visibleObject in VisibleTarget)
+            _visibleObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!(other.gameObject.tag == "Player"))
+            return;
+        foreach (GameObject _visibleObject in VisibleTarget)
+            _visibleObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
